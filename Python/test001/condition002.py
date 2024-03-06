184cf7e8e1907ce9e1d5825f6b4f7502aa17049f
kenHoribe <khoribe@r5.ucom.ne.jp>
2024-03-06 19:26:43 +0900

    elif追加

--- Python/test001/condition002.py
+++ Python/test001/condition002.py
@@ -2,5 +2,7 @@ old = 18
 
 if old < 20:
     print("20歳未満の方はご利用できませんよ。ヨーヨ hoge-ra!")
+elif 20 < old & old < 40:
+    print("maikka-")    
 else:
-    print("you are over 20 years old.")
+    print("いい年して。。。.")
