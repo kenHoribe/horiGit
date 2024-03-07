f66573ea012f753cd51262c5650f99cf68a1678c
kenHoribe <khoribe@r5.ucom.ne.jp>
2024-03-07 16:56:49 +0900

    split, split の応用

--- Python/test001/string_split.py
+++ Python/test001/string_split.py
@@ -29,3 +29,8 @@ print(s.strip('-+'))
 
 s = '-+- one -+-'
 print(s.strip('-+ '))
+
+s = 'one, two,  three'
+l = [x.strip() for x in s.split(',')]
+print(l)
+#ここでは、split()で文字列を分割して取得したリストにstrip()を適用する。
