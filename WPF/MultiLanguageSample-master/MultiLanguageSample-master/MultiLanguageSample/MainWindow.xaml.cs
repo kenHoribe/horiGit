a382b4da16ecf9ebcdede8db7c3aa4e086cbb162
kenHoribe <khoribe@r5.ucom.ne.jp>
2024-03-25 22:15:37 +0900

    initial commit

---
+++ WPF/MultiLanguageSample-master/MultiLanguageSample-master/MultiLanguageSample/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiLanguageSample {
+	/// <summary>
+	/// MainWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+			DataContext = new MainWindowModel() { LanguageIndex = 0 };
+		}
+
+		private string[] CultureList = { "ja-JP", "en-US", "zh-CN" };
+
+		private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			var bindData = DataContext as MainWindowModel;
+			ResourceService.Current.ChangeCulture(CultureList[bindData.LanguageIndex]);
+
+            bindData.MyImage = Properties.Resources.pikminImg;
+        }
+
+		class MainWindowModel: INotifyPropertyChanged {
+
+            private string _MyImage = (string)Application.Current.Resources["pikminImg"];
+
+            public int LanguageIndex { get; set; }
+
+            public string MyImage
+            {
+                get
+                {
+                    return _MyImage; 
+                }
+                set
+                {
+                    _MyImage = value;
+                    NotifyPropertyChanged("MyImage");
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            private void NotifyPropertyChanged(String info)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(info));
+                }
+            }
+
+        }
+	}
+}
---
+++ WPF/MultiLanguageSample-master/MultiLanguageSample-master/MultiLanguageSample/ResourceService.cs
@@ -0,0 +1,35 @@
+﻿using MultiLanguageSample.Properties;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace MultiLanguageSample {
+	public class ResourceService : INotifyPropertyChanged {
+		#region シングルトン対策
+		private static readonly ResourceService current = new ResourceService();
+		public static ResourceService Current => current;
+		#endregion
+
+		#region INotifyPropertyChanged対策
+		public event PropertyChangedEventHandler PropertyChanged;
+		protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null) {
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+		#endregion
+
+		/// <summary>
+		/// リソースを取得
+		/// </summary>
+		private readonly Resources resources = new Resources();
+		public Resources Resources => resources;
+
+		/// <summary>
+		/// リソースのカルチャーを変更
+		/// </summary>
+		/// <param name="name">カルチャー名</param>
+		public void ChangeCulture(string name) {
+			Resources.Culture = CultureInfo.GetCultureInfo(name);
+			RaisePropertyChanged("Resources");
+		}
+	}
+}
